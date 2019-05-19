8eeafae9ac89f06c4e30509fb3c8490055ac8e0c
axelvestberg <axel.vestberg@gmail.com>
2019-05-19 18:08:07 +0200

    added missingnumbertwo challenge folder

---
+++ missingNumberTwo/missingNumberTwo.js
@@ -0,0 +1,9 @@
+missingNumberTwo = arr => {
+	
+}
+
+//[...Array(10).keys()].map(x => x++);
+let arr = Array.from({length: 100}, (v, k) => k+1);
+console.log(arr);
+
+module.exports = missingNumberTwo;
